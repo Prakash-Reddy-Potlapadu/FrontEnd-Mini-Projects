fb1c0409dad45b2922eeb7201c8032b969de4223
Prakash Reddy <pprakash@mirabeltechnologies.com>
2021-10-26 23:34:19 +0530

    popup modal in vanilla html,css,js

---
+++ Vanilla-html-css-js/PopupModalWithBlurBackground/index.js
@@ -0,0 +1,12 @@
+function onOpenModalClick() {
+    document.getElementById("modal").style.display = "block";
+    document.body.style.backgroundColor = "#dfdfdf";
+    document.body.style.pointerEvents = "none";
+    document.getElementById("modal").style.pointerEvents = "all";
+}
+
+function onCloseModalClick() {
+    document.getElementById("modal").style.display = "none";
+    document.body.style.backgroundColor = "#ffff";
+    document.body.style.pointerEvents = "all";
+}
