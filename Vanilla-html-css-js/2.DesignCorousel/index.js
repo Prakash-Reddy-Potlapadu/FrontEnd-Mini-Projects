30bde412c5cf15a04e35e79c4c3dec86122786f7
Prakash Reddy <reddy.prakash874@gmail.com>
2024-07-20 20:26:41 +0530

    changes

--- Vanilla-html-css-js/2.DesignCorousel/index.js
+++ Vanilla-html-css-js/2.DesignCorousel/index.js
@@ -1,9 +1,9 @@
-let images, interval, imageNumber;
+let images, interval, imageNumber=1;
 
 window.addEventListener("load", () => {
-    images = document.getElementsByClassName("corousel-image");
-    interval = setInterval(slideLeft, 5000);
-    imageNumber = 0;
+    images = document.getElementsByClassName("carousel-image");
+    interval = setInterval(slideLeft, 1000); // to change
+    
 });
 
 function slideLeft() {
@@ -12,96 +12,92 @@ function slideLeft() {
     const third = document.getElementById("third");
     const fourth = document.getElementById("fourth");
     const fifth = document.getElementById("fifth");
-    //return () => {
         imageNumber++;
+        if(imageNumber>5){
+            imageNumber=1;
+        }
         for (let img of images) {
             let translate;
 
-            switch (imageNumber) {
+            switch(imageNumber){
                 case 1:
-                    translate = "translateX(-100%)";
+                    translate="translateX(0%)";
+                    first.classList.add("active");
+                    second.classList.remove("active");
+                    third.classList.remove("active");
+                    fourth.classList.remove("active");
+                    fifth.classList.remove("active");
+                    break;
+                case 2:
+                    translate="translateX(-100%)";
                     second.classList.add("active");
                     first.classList.remove("active");
                     third.classList.remove("active");
                     fourth.classList.remove("active");
                     fifth.classList.remove("active");
-
                     break;
-
-                case 2:
-                    translate = "translateX(-200%)";
+                case 3:
+                    translate="translateX(-200%)";
                     third.classList.add("active");
                     first.classList.remove("active");
                     second.classList.remove("active");
                     fourth.classList.remove("active");
                     fifth.classList.remove("active");
                     break;
-
-                case 3:
-                    translate = "translateX(-300%)";
+                    break;
+                case 4:
+                    translate="translateX(-300%)";
                     fourth.classList.add("active");
                     first.classList.remove("active");
                     second.classList.remove("active");
                     third.classList.remove("active");
                     fifth.classList.remove("active");
                     break;
-
-                case 4:
-                    translate = "translateX(-400%)";
+                 case 5:
+                    translate="translateX(-400%)";
                     fifth.classList.add("active");
                     first.classList.remove("active");
                     second.classList.remove("active");
                     third.classList.remove("active");
                     fourth.classList.remove("active");
-
                     break;
+               
 
-                default:
-                    translate = "translateX(0%)";
-                    first.classList.add("active");
-                    second.classList.remove("active");
-                    third.classList.remove("active");
-                    fourth.classList.remove("active");
-                    fifth.classList.remove("active");
-                    imageNumber = 0;
-                    break;
             }
 
             img.style.transform = translate;
         }
-    //};
 }
 
-//function slideRight() {}
 
-document.getElementsByClassName("corousel-container")[0].addEventListener("mouseenter", () => {
+document.getElementsByClassName("carousel-container")[0].addEventListener("mouseenter", () => {
     clearInterval(interval);
 });
-document.getElementsByClassName("corousel-container")[0].addEventListener("mouseleave", () => {
-    interval = setInterval(slideLeft, 5000);
+document.getElementsByClassName("carousel-container")[0].addEventListener("mouseleave", () => {
+    interval = setInterval(slideLeft, 1000);
 });
 
-function handleCorouselIndexClick(event) {
+function handlecarouselIndexClick(event) {
     const id = event.target.id;
     switch (id) {
         case "first":
-            imageNumber = -1;
-            slideLeft();
-            break;
-        case "second":
             imageNumber = 0;
             slideLeft();
             break;
-        case "third":
+        case "second":
             imageNumber = 1;
             slideLeft();
             break;
-        case "fourth":
+        case "third":
             imageNumber = 2;
             slideLeft();
             break;
-        case "fifth":
+        case "fourth":
             imageNumber = 3;
+            slideLeft();
+            break;
+        case "fifth":
+            imageNumber = 4;
             slideLeft();
             break;
         default:
